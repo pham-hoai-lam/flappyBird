45c375462cacb67ef96d662c57782e18b2a0c386
LamPH <phamlam090@gmail.com>
2020-01-24 13:08:16 +0700

    pipe moving

--- flappyBird.js
+++ flappyBird.js
@@ -28,11 +28,20 @@ function moveUp() {
     bY -= 20;
 }
 
-function draw() {
+var pipe = [];
+pipe[0] = {
+    x: cvs.width,
+    y: 0
+};
 
+function draw() {
     ctx.drawImage(bg, 0, 0);
-    ctx.drawImage(pipeNorth, 120, 0);
-    ctx.drawImage(pipeSouth, 120, 0 + cons);
+    for (var i = 0; i < pipe.length; i++) {
+        ctx.drawImage(pipeNorth, pipe[i].x, pipe[i].y);
+        ctx.drawImage(pipeSouth, pipe[i].x, pipe[i].y + cons);
+        pipe[i].x--;
+    }
+
     ctx.drawImage(fg, 0, cvs.height - fg.height);
     ctx.drawImage(bird, bX, bY);
     bY += gravity;
